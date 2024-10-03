cf60bc04dc139f495f8ecc88d6d599c4e78598ed
Jonathan Ellis <jbellis@datastax.com>
2024-10-03 11:09:31 -0500

    parameterize

--- tests/test_colbert_live.py
+++ tests/test_colbert_live.py
@@ -1,9 +1,15 @@
+import pytest
+
 from colbert_live import ColbertLive
 from colbert_live.models import ColpaliModel
-from .db import InMemoryDB
+from .db import InMemoryDB  # Use absolute import here
 
-def test_colpali():
-    model = ColpaliModel()
+@pytest.mark.parametrize("model_name", [
+    'vidore/colqwen2-v0.1',
+    'vidore/colpali-v1.2'
+])
+def test_colpali(model_name):
+    model = ColpaliModel(model_name)
     db = InMemoryDB(model)
     colbert = ColbertLive(db, model, 1, 0)
     for fname in db.encodings.keys():
